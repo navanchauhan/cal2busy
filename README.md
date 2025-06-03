da474d926ad2f257b67b0e9150c0c5ca8e5fbc61
Navan Chauhan <navanchauhan@gmail.com>
2025-06-02 21:39:26 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Busy2Cal
+
+Have you ever wanted to share your busy schedule with others, but found yourself juggling too many different calendar apps? Maybe you use iCloud and would have to share each calendar individually.
+
+Busy2Cal is a dead simple API service that merges all your calendars into a single iCalendar file endpoint. All events are titled “Busy.”
+
+## Usage
+
+Create a new file called `config.ini` and add your configuration:
+
+```
+[api]
+endpoint = C6623218E8464154379966D718F12
+base_path = /calendars
+
+[calendars]
+urls =
+    https://calendar.google.com/calendar/ical/primary@gmail.com/public/basic.ics
+    webcal://p62-caldav.icloud.com/published/2/somerandomstring
+```
+
+Then, simply run the following command:
+
+```bash
+uvicorn main:app
+```
+
+Now, you can access your merged calendar at `http://localhost:8000/calendars/C6623218E8464154379966D718F12/cal.ics`.
---
+++ main.py
@@ -0,0 +1,68 @@
+import configparser
+import icalendar
+import requests
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+endpoint = config.get('api', 'endpoint')
+base_path = config.get('api', 'base_path')
+
+if base_path.endswith("/") and len(base_path) > 1:
+    base_path = base_path[:-1]
+
+print(f"Merged calendar will be available at {base_path}/{endpoint}/cal.ics")
+
+app = FastAPI(
+    root_path=base_path,
+    docs_url=None,
+    redoc_url=None,
+    openapi_url=None
+)
+
+def get_calendar_urls():
+    links = config.get('calendars', 'urls').splitlines()
+    return [link.strip() for link in links if link != '']
+
+def merge_calendars(urls):
+    merged_calendar = icalendar.Calendar()
+    merged_calendar.add('prodid', '-//cal2busy//')
+    merged_calendar.add('version', '2.0')
+
+    keys_to_keep = [
+        'UID', 'DTSTART', 'DTEND', 'RRULE', 'EXDATE',
+        'RECURRENCE-ID', 'SUMMARY', 'DTSTAMP', 'SEQUENCE'
+    ]
+
+    extra_components_to_keep = [
+        # "DAYLIGHT", "STANDARD", "VCALENDAR"
+    ]
+
+    for url in urls:
+        if url.startswith('webcal'):
+            url = url.replace('webcal', 'http')
+        response = requests.get(url)
+        if response.status_code == 200:
+            calendar = icalendar.Calendar.from_ical(response.content)
+            for component in calendar.walk():
+                if component.name == 'VEVENT':
+                    new_event = icalendar.Event()
+                    for key in keys_to_keep:
+                        if key in component:
+                            new_event[key] = component[key]
+                    new_event['SUMMARY'] = 'Busy'
+                    merged_calendar.add_component(new_event)
+                elif component.name in extra_components_to_keep:
+                    merged_calendar.add_component(component)
+    return merged_calendar
+
+@app.get(f"/{endpoint}/cal.ics", response_class=StreamingResponse)
+def get_merged_calendar():
+    urls = get_calendar_urls()
+    merged_calendar = merge_calendars(urls)
+    ics_bytes = merged_calendar.to_ical()
+    return StreamingResponse(BytesIO(ics_bytes), media_type="text/calendar", headers={
+        "Content-Disposition": "attachment; filename=merged_calendar.ics"
+    })
